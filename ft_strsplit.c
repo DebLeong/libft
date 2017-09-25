ce2c61665b023a81c9af89211e538211b14c3048
Deborah Leong <dleong@e1z2r2p11.42.us.org>
2017-09-25 13:24:20 -0700

    replacing ft_strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,37 +6,36 @@
 /*   By: dleong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/23 17:22:04 by dleong            #+#    #+#             */
-/*   Updated: 2017/09/25 13:08:35 by dleong           ###   ########.fr       */
+/*   Updated: 2017/09/25 13:23:16 by dleong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#define G(x) ft_memalloc(x)
 
-char				**ft_strsplit(char const *s, char c)
+char	**ft_strsplit(char const *s, char c)
 {
-	size_t			j[2];
-	unsigned int	a;
-	char			**r;
-
-	r = NULL;
+	char    **ptrptr;
+	int     cw;
+	int     i;
+	ptrptr = NULL;
 	if (s)
 	{
-		j[0] = -1;
-		j[1] = -1;
-		if (!(r = (char **)G(sizeof(char*) * (ft_gc(s, c) + 1))))
+		i = 0;
+		cw = ft_gc(s, c);
+		ptrptr = (char **)malloc((sizeof(char *) * cw) + 1);
+		if (!ptrptr)
 			return (NULL);
-		while (s[++j[0]] != '\0' && ++j[1] < (size_t)ft_gc(s, c))
+		while (cw--)
 		{
-			while (s[j[0]] == c)
-				j[0]++;
-			a = j[0];
-			while (s[j[0]] != c)
-				j[0]++;
-			if (!(r[j[1]] = (char *)ft_strsub((char *)s, a, ft_gl(&s[a], c))))
-				ft_memdel((void**)r);
+			while (*s == c && *s != '\0')
+				s++;
+			ptrptr[i] = ft_strsub(s, 0, ft_gl(s, c));
+			if (!ptrptr[i])
+				return (NULL);
+			s = s + ft_gl(s, c);
+			i++;
 		}
-		r[j[1]] = NULL;
+		ptrptr[i] = NULL;
 	}
-	return (r);
+	return (ptrptr);
 }
